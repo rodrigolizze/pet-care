a45250e98bbdf34a86e63ca98cacfab50f7c9ff0
Paulo Cruz <peagafer@gmail.com>
2025-08-08 00:56:32 +0100

    Home with search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,21 @@
 class PagesController < ApplicationController
   def home
-    @sitters = User.includes(:availabilities).where(role: "sitter")
+    @city = params[:city]
+    @date = params[:date]
+
+    sitters = User.where(role: "sitter")
+
+    if @city.present?
+      sitters = sitters.where("address ILIKE ?", "%#{@city}%")
+    end
+
+    if @date.present?
+      begin
+        search_date = Date.parse(@date)
+        sitters = sitters.joins(:availabilities).where(availabilities: { date: search_date }).distinct
+      end
+    end
+
+    @sitters = sitters.includes(:availabilities)
   end
 end
