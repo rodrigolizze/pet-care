54decc89a19b465b2c4e059077f8971545738fe4
Rodrigo Lizze <rodrigolizze.arq@gmail.com>
2025-08-07 21:10:27 -0300

    Bunch of controllers

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -3,7 +3,7 @@ class AvailabilitiesController < ApplicationController
   before_action :require_sitter
 
   def index
-    @availabilities = current_user.availabilities
+    @availabilities = current_user.availabilities.order(:date)
   end
 
   def new
@@ -11,13 +11,32 @@ class AvailabilitiesController < ApplicationController
   end
 
   def create
-    @availability = current_user.availabilities.build(availability_params)
-    if @availability.save
-      redirect_to availabilities_path, notice: "Dispomibilidade criada com sucesso."
-    else
-      render :new
+    start_date = Date.parse(params[:start_date])
+    end_date = Date.parse(params[:end_date])
+
+    if end_date < start_date
+      redirect_to new_availability_path, alert: "A data de término não pode ser anterior à data de início."
+      return
     end
+
+    created = 0
+
+    (start_date..end_date).each do |date|
+      current_user.availabilities.find_or_create_by(date: date)
+      created += 1
+    end
+
+    redirect_to availabilities_path, notice: "#{created} disponibilidade(s) criada(s) com sucesso."
+  rescue ArgumentError
+    redirect_to new_availability_path, alert: "Datas inválidas."
   end
+
+  def destroy
+    @availability = current_user.availabilities.find(params[:id])
+    @availability.destroy
+    redirect_to availabilities_path, notice: "Disponibilidade excluída."
+  end
+
 
   private
 
